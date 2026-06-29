7184f23beb045c9b9aff66dc431a85fd6db1329e
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-06-29 10:19:40 +0200

    feat(ex01): add a function to compare string to n

---
+++ ex01/ft_strncmp.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nfurst <nfurst@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/27 14:54:52 by nfurst            #+#    #+#             */
+/*   Updated: 2026/06/29 10:17:42 by nfurst           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <stdio.h>
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	while (n > 0 && (*s1 != '\0' || *s2 != '\0'))
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+		n--;
+	}	
+	return (0);
+}
+
+/*
+int main(void)
+{
+	int diff;
+
+	diff = 0;
+	diff = ft_strncmp("Hello", "HelloWolrd", 2);
+	printf("%d", diff);
+	return (0);
+}
+*/
