425455cbfaf57c59addfcda5576df78bd2f57752
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-06-29 13:45:29 +0200

    feat(ex05): add a function to copy string to it

---
+++ ex05/ft_strlcat.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nfurst <nfurst@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/29 11:58:57 by nfurst            #+#    #+#             */
+/*   Updated: 2026/06/29 13:44:34 by nfurst           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	dest_len;
+	unsigned int	src_len;
+
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	if (size <= dest_len)
+		return (size + src_len);
+	i = dest_len;
+	j = 0;
+	while (src[j] != '\0' && i < size - 1)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest_len + src_len);
+}
+
+/*
+#include <stdio.h>
+
+int main(void)
+{
+	char dest[] = "1337 42";
+	char src[] = "Born to code";
+
+	printf("Return: %d \n", ft_strlcat(dest, src, 7));
+	printf("%s \n", dest);
+	return(0);
+}
+*/
